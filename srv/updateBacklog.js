7891afa4531820407a32ea8955fa40cf5205c19a
Shivam Agarwal <shivamagarwal36029@gmail.com>
2024-04-15 18:10:38 +0530

    validated updateBacklog

--- srv/updateBacklog.js
+++ srv/updateBacklog.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
 const listBacklogs = require('./listBacklogs');
+const {throwNameValidationError, noValidBacklogFoundError}  = require('./utils/errors')
+
 function updateBacklog(name, status){
     const tasksJSON = fs.readFileSync('./backlog-db/backlogs.json', 'utf8');
     const tasks = JSON.parse(tasksJSON);
-
+    if(!name || typeof name !== 'string' || name.startsWith("-")) throwNameValidationError();
+    
     const indexToUpdate = tasks.findIndex(obj => obj.Name.toLowerCase() === name.toLowerCase());
 
     if (indexToUpdate !== -1) {
         tasks[indexToUpdate].Status = status;
         console.log("Successfuly updated");
     } else {
-        console.log("Backlog with name", name, "not found.");
+        listBacklogs();
+        noValidBacklogFoundError(name);
     }
     const updatedTasksJSON = JSON.stringify(tasks);
     fs.writeFileSync('./backlog-db/backlogs.json', updatedTasksJSON);
