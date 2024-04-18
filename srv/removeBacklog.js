ffaf4a0727410a3fbcb372a3d7d622f17f291f78
Shivam Agarwal <shivamagarwal36029@gmail.com>
2024-04-18 15:23:47 +0530

    added github for upd/rem

--- srv/removeBacklog.js
+++ srv/removeBacklog.js
@@ -2,37 +2,34 @@ const fs = require('fs');
 const listBacklogs = require('./listBacklogs');
 const {throwNameValidationError, noValidBacklogFoundError}  = require('./utils/errors')
 const {writeToFile, readFromFile} = require('./utils/fileUtility')
+const {issueMarkAsClosed} = require("./utils/githubUtility")
 
-function removeBacklog(options){
+function removeBacklog(options,system){
     if(system == 0){
         const tasksJSON = readFromFile();
         const tasks = JSON.parse(tasksJSON);
-    }
-    else{
-        // Code to fetch from Github and parse
-    }
-
-    if(!options.name || 
-        typeof options.name !== 'string' || 
-        options.name.startsWith("-")) throwNameValidationError();
-    
-    const updTasks = tasks.filter(obj => obj.Name.toLowerCase() !== options.name.toLowerCase());
-    
-    if (tasks.length > updTasks.length){
-        console.log("Successfully removed task..");
-        if(system == 0){
+        if(!options.name || 
+            typeof options.name !== 'string' || 
+            options.name.startsWith("-")) throwNameValidationError();
+        
+        const updTasks = tasks.filter(obj => obj.Name.toLowerCase() !== options.name.toLowerCase());
+        
+        if (tasks.length > updTasks.length){
+            console.log("Successfully removed task..");
             const updatedTasksJSON = JSON.stringify(updTasks);
             writeToFile(updatedTasksJSON);
         }
-        else{
-            // Code to add to Github
+        else {
+            noValidBacklogFoundError(options.name)
         }
+        
+        listBacklogs();
     }
-    else {
-        noValidBacklogFoundError(options.name)
+    else{
+        issueMarkAsClosed(options.name,"not_planned")
     }
+
     
-    listBacklogs();
 }
 
 module.exports = removeBacklog
--- srv/updateBacklog.js
+++ srv/updateBacklog.js
@@ -3,33 +3,30 @@ const listBacklogs = require('./listBacklogs');
 const {throwNameValidationError, noValidBacklogFoundError}  = require('./utils/errors')
 const {writeToFile, readFromFile} = require('./utils/fileUtility')
 
-function updateBacklog(name, status){
+function updateBacklog(name, status, system){
     if(system == 0){
         const tasksJSON = readFromFile();
         const tasks = JSON.parse(tasksJSON);
-    }
-    else{
-        // Code to fetch from Github and parse
-    }
-    if(!name || typeof name !== 'string' || name.startsWith("-")) throwNameValidationError();
+        if(!name || typeof name !== 'string' || name.startsWith("-")) throwNameValidationError();
     
-    const indexToUpdate = tasks.findIndex(obj => obj.Name.toLowerCase() === name.toLowerCase());
+        const indexToUpdate = tasks.findIndex(obj => obj.Name.toLowerCase() === name.toLowerCase());
 
-    if (indexToUpdate !== -1) {
-        tasks[indexToUpdate].Status = status;
-        console.log("Successfuly updated");
-    } else {
-        listBacklogs();
-        noValidBacklogFoundError(name);
-    }
-    if(system == 0){
+        if (indexToUpdate !== -1) {
+            tasks[indexToUpdate].Status = status;
+            console.log("Successfuly updated");
+        } else {
+            listBacklogs();
+            noValidBacklogFoundError(name);
+        }
         const updatedTasksJSON = JSON.stringify(tasks);
         writeToFile(updatedTasksJSON);
+
+        listBacklogs();
     }
     else{
-        // Code to add to Github
+        issueMarkAsClosed(options.name,"completed")
     }
-    listBacklogs();
+    
 }
 
 module.exports = updateBacklog
